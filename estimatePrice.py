f97d47dbd1ba9654a13cf12e9060fa51d033ab0d
Martin LEROY <maleroy@student.42.fr>
2019-10-16 13:30:52 +0200

    estimatePrice + linearRegression

--- estimatePrice.py
+++ estimatePrice.py
@@ -1,20 +1,41 @@
 import sys
 import pickle
 
-if __name__ == "__main__":
+def checkInt(i):
+    try:
+        int(i)
+    except:
+        return False
+    return True
+
+def getTheta():
     try:
         with open('cache', 'rb') as fichier:
             my_unpickler = pickle.Unpickler(fichier)
-            tetha0 = my_unpickler.load()
-            tetha1 = my_unpickler.load()
-            print(tetha0, tetha1, sep="-")
+            theta0 = my_unpickler.load()
+            theta1 = my_unpickler.load()
             fichier.close()
     except:
-        pass
-    tetha0 = 0
-    tetha1 = 1
+        theta0 = 0
+        theta1 = 0
+    return (theta0, theta1)
+
+def setTheta(theta0, theta1):
     with open('cache', 'wb') as fichier:
         my_pickler = pickle.Pickler(fichier)
-        my_pickler.dump(tetha0)
-        my_pickler.dump(tetha1)
+        my_pickler.dump(theta0)
+        my_pickler.dump(theta1)
         fichier.close()
+
+def estimatePrice(mileage):
+    theta0, theta1 = getTheta()
+    return theta0 + theta1 * mileage
+
+if __name__ == "__main__":
+    theta0, theta1 = getTheta()
+    if theta0 == 0 and theta1 == 0:
+        setTheta(theta0, theta1)
+    if len(sys.argv) == 2 and checkInt(sys.argv[1]):
+        print(estimatePrice(int(sys.argv[1])))
+    else:
+        print("usage :\testimatePrice.py \"mileage\"\n\t- mileage must be an Int value")
--- ft_linear_regression.py
+++ ft_linear_regression.py
@@ -1,35 +1,47 @@
-import sys
-import pickle
 import csv
+import collections
+from estimatePrice import getTheta, setTheta, estimatePrice, checkInt
 
-if __name__ == "__main__":
-    try:
-        with open('cache', 'rb') as fichier:
-            my_unpickler = pickle.Unpickler(fichier)
-            tetha0 = my_unpickler.load()
-            tetha1 = my_unpickler.load()
-            print(tetha0, tetha1, sep="-")
-            fichier.close()
-    except:
-        pass
-    tetha0 = 41
-    tetha1 = 42
-    with open('cache', 'wb') as fichier:
-        my_pickler = pickle.Pickler(fichier)
-        my_pickler.dump(tetha0)
-        my_pickler.dump(tetha1)
-        fichier.close()
+def readData():
+    line = -1
     try:
         with open('data.csv') as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=',')
-            line = 0
+            data = collections.OrderedDict()
             for row in csv_reader:
-                if line == 0:
+                if line == -1:
                     print(f'Column names are {", ".join(row)}')
                     line += 1
                 else:
-                    print(f'\t{row[0]}km = {row[1]}$')
-                    line += 1
+                    if len(row) == 2 and checkInt(row[0]) and checkInt(row[1]):
+                        data[row[0]] = row[1]
+                        line += 1
+            print(f'Processed {line} lines')
+            csv_file.close()
     except:
-        line = 0
-    print(f'Processed {line} lines.')
+        pass
+    if line > 0:
+        return data
+    return None
+
+def linearRegression(data):
+    m = 0
+    learningRate = 0.5
+    tmpT0 = 0
+    tmpT1 = 0
+    for km, price in data.items():
+        tmpT0 += estimatePrice(int(km)) - int(price)
+        tmpT1 += (estimatePrice(int(km)) - int(price)) * int(km)
+        m += 1
+    tmpT0 = tmpT0 * learningRate * (1/m)
+    tmpT1 = tmpT1 * learningRate * (1/m)
+    print("new theta :", tmpT0, tmpT1, sep="  ")
+    setTheta(tmpT0, tmpT1)
+
+if __name__ == "__main__":
+    theta0, theta1 = getTheta()
+    if theta0 == 0 and theta1 == 0:
+        setTheta(theta0, theta1)
+    data = readData()
+    if data:
+        linearRegression(data)
